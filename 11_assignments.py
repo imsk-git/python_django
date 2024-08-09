7c3df8d0e93d15da6c5931655b7af87522554f53
Shreya Karn <skarn6469@gmail.com>
2024-08-09 21:46:31 +0545

    Calculator code added

---
+++ 11_assignments.py
@@ -0,0 +1,33 @@
+#WAP to make a calculator.
+
+
+num1 = input("Enter first digit: ")
+if num1.replace('.','',1).isdigit():
+    num1 = float(num1)
+    operator = input("Enter operator(+,-,*,/): ")
+    if operator in ['+','-','*','/']:
+        num2 = input("Enter second digit: ")
+        if num2.replace('.','',1).isdigit():
+            num2 = float(num2)
+            if operator=='+':
+                print(num1, "+", num2, "=", num1+num2)
+            elif operator=='-':
+                print(num1, "-", num2, "=",num1-num2)
+            elif operator=='*':
+                print(num1, "*", num2, "=",num1*num2)
+            elif operator=='/':
+                if num2!=0:
+                    print(num1, "/", num2, "=",num1/num2)
+                else:
+                    print("Can't divide by zero.")
+        else:
+            print("Invalid input. Please enter a number.")
+    else:
+        print("Invalid choice")
+else:
+    print("Invalid input. Please enter a number.")
+
+
+
+
+
---
+++ 2_variable.py
@@ -0,0 +1,11 @@
+x = 5
+y = 'skillshikshya'
+print(x)
+print(y)
+
+x = str(3)
+y = int(5)
+z = float(10)
+print(x)
+print(y)
+print(z)
